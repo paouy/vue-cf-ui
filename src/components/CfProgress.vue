269490ac1b7224ddb70b04fd84c936f4e02d94f8
paoloernestouy <paolo.uy@obf.ateneo.edu>
2022-10-22 23:31:21 +0800

    Add CfProgress and add CfDropZone cancel event

---
+++ src/components/CfProgress.vue
@@ -0,0 +1,41 @@
+<script setup>
+const props = defineProps({
+  value: {
+    type: Number,
+    default: 0
+  },
+  max: {
+    type: Number,
+    default: 100
+  }
+})
+</script>
+
+<template>
+  <progress
+    class="cf-progress"
+    :value="props.value"
+    :max="props.max"
+  >
+    {{ value }}%
+  </progress>
+</template>
+
+<style lang="scss">
+.cf-progress {
+  width: 100%;
+  height: 1rem;
+
+  &::-webkit-progress-value {
+    background-color: var(--cf-blue-4);
+  }
+
+  &::-webkit-progress-bar {
+    background-color: var(--cf-gray-8);
+  }
+
+  &::-moz-progress-bar {
+    background-color: var(--cf-blue-4);
+  }
+}
+</style>
